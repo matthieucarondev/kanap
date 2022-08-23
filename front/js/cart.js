02b44a5949a9dceb47015ec667bb492c7a0b9ccf
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2022-08-23 15:33:11 +0200

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,69 +1,246 @@
-let addProduit = JSON.parse(localStorage.getItem("addProduit"));
+const cart = []
 
-//transfert panier si il existe
-async function fetchbasket() {
-  
-  let qtyTotal = 0;
-  let priceTotal = 0;
-  if (addProduit != null) {
-    for (let i = 0; i < addProduit.length; i++) {
-      let id = addProduit[i].id;
-      let color = addProduit[i].color;
-      let basketCart = document.querySelector(`#cart__items`);
-      let urlIdProduit = "http://localhost:3000/api/products/" + id;
-      //affiché produit
-      const response = await fetch(urlIdProduit);
+retrieveLocolStorage()
+cart.forEach((item) => displayItem(item))
 
-      if (!response.ok) {
-        let errProduit = `<article class="cart__item">
-  <spam> Il y a une erreur dans votre panier!</spam></article>`;
-        const dom = new DOMParser();
-        const displayErrProduit = dom.parseFromString(errProduit, "text/html");
-        basketCart.appendChild(displayErrProduit.body.firstChild);
-      } else {
-        const data = await response.json();
-        const Dom = new DOMParser();
-        const descrProduit = `<article class="cart__item" data-id="${id}" data-color="${color}">
-                 <div class="cart__item__img">
-                <img src="${data.imageUrl}" alt="${data.altTxt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${data.name}</h2>
-                    <p>${color}</p>
-                    <p data-id="price-${id}-${color}">Prix : ${data.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${addProduit[i].qty}">
-                    </div>
-                    <div cass="cart__item__content__settings__delete">
-                      <p class="deleteItem" onclick="deleteItem('${id}', '${color}', '${data.price}','${addProduit[i].qty}')>Supprimer</p>
-                    </div>
-                      </div>
-               </div>
-              </article>`;
-        const displayProduitDescrition = Dom.parseFromString(
-          descrProduit,
-          "text/html"
-        );
-        basketCart.appendChild(displayProduitDescrition.body.firstChild);
-        //quantité total
-        qtyTotal += parseInt(addProduit[i].qty);
-        document.querySelector("#totalQuantity").innerHTML = qtyTotal;
-        // prix total
-        priceTotal += data.price *addProduit[i].qty;
-        document.querySelector("#totalPrice").innerHTML = priceTotal;
-      }
-    }
-  } else {
-    //message si panier vide
-    document.querySelector(`h1`).innerText = `Votre panier est vide !`;
-    //message si 0 quantité
-    document.querySelector("#totalQuantity").innerHTML = "0";
-    //message si 0 prix
-    document.querySelector("#totalPrice").innerHTML = "0";
+const orderButton = document.querySelector("#order")
+orderButton.addEventListener("click", (e) => submitForm(e))
+// récupérer localstorage
+function retrieveLocolStorage() {
+  const numberOfItems = localStorage.length
+  for (let i = 0 ; i < numberOfItems; i++) {
+    const item = localStorage.getItem(localStorage.key(i)) || ""
+    const itemObject = JSON.parse(item)
+    cart.push(itemObject)
   }
 }
-fetchbasket();
+//afficher produit
+
+function displayItem(item) {
+  const article = makeArticle(item)
+  const imageDiv = makeImageDiv(item)
+  article.appendChild(imageDiv)
+  const cardItemContent = makeCartContent(item)
+  article.appendChild(cardItemContent)
+  displayArticle(article)
+  displayTotalQuantity()
+  displayTotalPrice()
+}
+//changer quantity total
+function displayTotalQuantity() {
+  const totalQuantity = document.querySelector("#totalQuantity")
+  const total = cart.reduce((total, item) => total + item.quantity, 0)
+  totalQuantity.textContent = total
+}
+//changer prix totla
+
+function displayTotalPrice() {
+  const totalPrice = document.querySelector("#totalPrice")
+  const total = cart.reduce((total, item) => total + item.price * item.quantity, 0)
+  totalPrice.textContent = total
+}
+// afficher contenant
+function makeCartContent(item) {
+  const cardItemContent = document.createElement("div")
+  cardItemContent.classList.add("cart__item__content")
+
+  const description = makeDescription(item)
+  const settings = makeSettings(item)
+
+  cardItemContent.appendChild(description)
+  cardItemContent.appendChild(settings)
+  return cardItemContent
+}
+//Afficher div
+function makeSettings(item) {
+  const settings = document.createElement("div")
+  settings.classList.add("cart__item__content__settings")
+
+  addQuantityToSettings(settings, item)
+  addDeleteToSettings(settings, item)
+  return settings
+}
+// supprimé 
+function addDeleteToSettings(settings, item) {
+  const div = document.createElement("div")
+  div.classList.add("cart__item__content__settings__delete")
+  div.addEventListener("click", () => deleteItem(item))
+
+  const p = document.createElement("p")
+  p.textContent = "Supprimer"
+  div.appendChild(p)
+  settings.appendChild(div)
+}
+function deleteItem(item) {
+  const itemToDelete = cart.findIndex(
+    (product) => product.id === item.id && product.color === item.color
+  )
+  cart.splice(itemToDelete, 1)
+  displayTotalPrice()
+  displayTotalQuantity()
+  deleteDataFromCache(item)
+  deleteArticleFromPage(item)
+}
+function deleteArticleFromPage(item) {
+  const articleToDelete = document.querySelector(
+    `article[data-id="${item.id}"][data-color="${item.color}"]`
+  )
+  articleToDelete.remove()
+}
+
+function addQuantityToSettings(settings, item) {
+  const quantity = document.createElement("div")
+  quantity.classList.add("cart__item__content__settings__quantity")
+  const p = document.createElement("p")
+  p.textContent = "Qté : "
+  quantity.appendChild(p)
+  const input = document.createElement("input")
+  input.type = "number"
+  input.classList.add("itemQuantity")
+  input.name = "itemQuantity"
+  input.min = "1"
+  input.max = "100"
+  input.value = item.quantity
+  input.addEventListener("input", () => updatePriceAndQuantity(item.id, input.value, item))
+
+  quantity.appendChild(input)
+  settings.appendChild(quantity)
+}
+
+function updatePriceAndQuantity(id, newValue, item) {
+  const itemToUpdate = cart.find((item) => item.id === id)
+  itemToUpdate.quantity = Number(newValue)
+  item.quantity = itemToUpdate.quantity
+  displayTotalQuantity()
+  displayTotalPrice()
+  saveNewDataToCache(item)
+}
+
+function deleteDataFromCache(item) {
+  const key = `${item.id}-${item.color}`
+  localStorage.removeItem(key)
+}
+
+function saveNewDataToCache(item) {
+  const dataToSave = JSON.stringify(item)
+  const key = `${item.id}-${item.color}`
+  localStorage.setItem(key, dataToSave)
+}
+
+function makeDescription(item) {
+  const description = document.createElement("div")
+  description.classList.add("cart__item__content__description")
+
+  const h2 = document.createElement("h2")
+  h2.textContent = item.name
+  const p = document.createElement("p")
+  p.textContent = item.color
+  const p2 = document.createElement("p")
+  p2.textContent = item.price + " €"
+
+  description.appendChild(h2)
+  description.appendChild(p)
+  description.appendChild(p2)
+  return description
+}
+
+function displayArticle(article) {
+  document.querySelector("#cart__items").appendChild(article)
+}
+function makeArticle(item) {
+  const article = document.createElement("article")
+  article.classList.add("card__item")
+  article.dataset.id = item.id
+  article.dataset.color = item.color
+  return article
+}
+//afficher image
+function makeImageDiv(item) {
+  const div = document.createElement("div")
+  div.classList.add("cart__item__img")
+
+  const image = document.createElement("img")
+  image.src = item.imageUrl
+  image.alt = item.altTxt
+  div.appendChild(image)
+  return div
+}
+
+function submitForm(e) {
+  e.preventDefault()
+  if (cart.length === 0) {
+    alert("Please select items to buy")
+    return
+  }
+
+  if (isFormInvalid()) return
+  if (isEmailInvalid()) return
+
+  const body = makeRequestBody()
+  fetch("http://localhost:3000/api/products/order", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const orderId = data.orderId
+      window.location.href = "/html/confirmation.html" + "?orderId=" + orderId
+    })
+    .catch((err) => console.error(err))
+}
+
+function isEmailInvalid() {
+  const email = document.querySelector("#email").value
+  const regex = /^[A-Za-z0-9+_.-]+@(.+)$/
+  if (regex.test(email) === false) {
+    alert("Please enter valid email")
+    return true
+  }
+  return false
+}
+
+function isFormInvalid() {
+  const form = document.querySelector(".cart__order__form")
+  const inputs = form.querySelectorAll("input")
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      alert("Please fill all the fields")
+      return true
+    }
+    return false
+  })
+}
+
+function makeRequestBody() {
+  const form = document.querySelector(".cart__order__form")
+  const firstName = form.elements.firstName.value
+  const lastName = form.elements.lastName.value
+  const address = form.elements.address.value
+  const city = form.elements.city.value
+  const email = form.elements.email.value
+  const body = {
+    contact: {
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      city: city,
+      email: email
+    },
+    products: getIdsFromCache()
+  }
+  return body
+}
+
+function getIdsFromCache() {
+  const numberOfProducts = localStorage.length
+  const ids = []
+  for (let i = 0; i < numberOfProducts; i++) {
+    const key = localStorage.key(i)
+    const id = key.split("-")[0]
+    ids.push(id)
+  }
+  return ids
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,107 +1,96 @@
-
-// recupération  de la chaine  de requéte url
-const wls =window.location.search;
-const URLsearchPar= new URLSearchParams(wls);
-const id = URLsearchPar.get(`id`);
-let urlId = 'http://localhost:3000/api/products/' + id;
-
-fetch(urlId)
-    .then((reponse) => reponse.json()
-    .then((data) => {
-//insert image
-  const img =document.querySelector(".item__img");
-  img.innerHTML = ` <img src="${data.imageUrl}" alt="${data.altTxt}"/>`;
-//insert h1
-  let H1 = document.querySelector("#title");
-  H1.innerHTML = `${data.name}`;
-//insert prix
-  let Price = document.querySelector("#price");
-  Price.innerHTML = `${data.price}`;
-//insert description
-  let descr = document.querySelector("#description");
-  descr.innerHTML = `${data.description}`;
-//insert color
-      const dom = new DOMParser();
-        const color= document.querySelector('#colors');
-
-        for ( i = 0; i < data.colors.length ; i++) {
-          const colorProduit =
-           `<option value='${data.colors[i]}'>${data.colors[i]}</option>`;
-           const choiceColors =dom.parseFromString(colorProduit,"text/html");
-           color.appendChild(choiceColors.body.firstChild);
-         
-        }
-  }))
-  .catch((err) =>
-  document.getElementById('items').innerText = `le produit est introuvable !`);
-
-  //enregistre la couleur choisi
-function valueColor(){
-  const color = document.querySelector('#colors');
-  return color.value ;
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const id = urlParams.get("id")
+if (id != null) {
+  let itemPrice = 0
+  let imgUrl, altText, articleName
 }
-  //enregistre la quantité choisi
-function valueQty(){
-  const qty = document.querySelector('#quantity');
-  return qty.value ;
+//recupére api
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((response) => response.json())
+  .then((res) => handleData(res))
+
+function handleData(duck) {
+  const { altTxt, colors, description, imageUrl, name, price } = duck
+  itemPrice = price
+  imgUrl = imageUrl
+  altText = altTxt
+  articleName = name
+  makeImage(imageUrl, altTxt)
+  makeTitle(name)
+  makePrice(price)
+  makeDescription(description)
+  makeColors(colors)
 }
-
-
-//envoyer la couleur et la quantity au localStorage
- const addToCartproduit= (id, color, qty) => {
-  //pas de color choisi ni quantity'erreur'
-  if (color=="" && qty=="0") {
-    return alert(`Veuillez choisir une couleur et une quantité entre 1 et 100`)
+//afficher image
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img")
+  image.src = imageUrl
+  image.alt = altTxt
+  const parent = document.querySelector(".item__img")
+  if (parent != null) parent.appendChild(image)
+}
+//afficher title
+function makeTitle(name) {
+  const h1 = document.querySelector("#title")
+  if (h1 != null) h1.textContent = name
+}
+//afficher prix
+function makePrice(price) {
+  const span = document.querySelector("#price")
+  if (span != null) span.textContent = price
+}
+//afficher description
+function makeDescription(description) {
+  const p = document.querySelector("#description")
+  if (p != null) p.textContent = description
+}
+//selectionner couleur
+function makeColors(colors) {
+  const select = document.querySelector("#colors")
+  if (select != null) {
+    colors.forEach((color) => {
+      const option = document.createElement("option")
+      option.value = color
+      option.textContent = color
+      select.appendChild(option)
+    })
   }
-  //pas de couleur erreur
-  if (color=="") {
-    return alert(`Veuillez choisir une couleur`)
+}
+// boutton 
+const button = document.querySelector("#addToCart")
+button.addEventListener("click", handleClick)
+
+function handleClick() {
+  const color = document.querySelector("#colors").value
+  const quantity = document.querySelector("#quantity").value
+
+  if (isOrderInvalid(color, quantity)) return
+  saveOrder(color, quantity)
+  redirectToCart()
+}
+//enregistré localstorage
+
+function saveOrder(color, quantity) {
+  const key = `${id}-${color}`
+  const data = {
+    id: id,
+    color: color,
+    quantity: Number(quantity),
+    price: itemPrice,
+    imageUrl: imgUrl,
+    altTxt: altText,
+    name: articleName
   }
-  // quantity non comprise entre 0 et 100 "erreur"
-  if (qty<=0 ||qty>=101) {
-    return alert(`Veuillez une quantité entre 1 et 100`)
-  } 
- 
-  //si panier vide => créer panier
-  let produitLocalStorage = addCart();
-  if (produitLocalStorage.length == 0 ) {
-    produitLocalStorage=[{id: id, color: color, qty: qty}];
-    //panier deja creer
-    } else {
-      let basket = false;
-      //produit et couleur existe déjà additionné les quantité
-      for (let i= 0; i < produitLocalStorage.length; i++) {
-       if (id === produitLocalStorage[i].id && color === produitLocalStorage[i].color) {
-         basket  = true;
-        produitLocalStorage[i].qty += qty;
-  
-        }
-      }
-      //si il n'existe pas le creer
-      if (basket==false) {
-        const produit = {id:id,color:color,qty:qty};
-      produitLocalStorage.push(produit);
-      }
- }
-      localStorage.setItem(`addProduit`, JSON.stringify(produitLocalStorage));
-      alert(`produit(s) ajouté(s) au panier!`);
-   
+  localStorage.setItem(key, JSON.stringify(data))
+}
+//erreur si coulleur ou nombre nul
+function isOrderInvalid(color, quantity) {
+  if (color == null || color === "" || quantity == null || quantity == 0) {
+    alert("Please select a color and quantity")
+    return true
   }
- // Bouton d'ajouter
-
-let addToCart = document.querySelector(`#addToCart`);
-
-addToCart.addEventListener(`click`, () => {
- 
-    let color = valueColor();
-    let qty = parseInt(valueQty());
-    addToCartproduit( id, color, qty);
-
- });
-const addCart = () => {   
-    let produitLocalStorage  = [];
-    if (localStorage.getItem(`addProduit`) != null) { 
-        produitLocalStorage = JSON.parse(localStorage.getItem(`addProduit`));
-    }
-    return produitLocalStorage ;}
-
+}
+function redirectToCart() {
+  window.location.href = "cart.html"
+}
